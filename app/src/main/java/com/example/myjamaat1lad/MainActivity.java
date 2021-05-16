ba10904615506dafe62da36401f0be3562a74d8b
Faisal Raim <fhr1309@gmail.com>
2021-05-16 17:20:35 +0600

    Added list

--- app/src/main/java/com/example/myjamaat1lad/MainActivity.java
+++ app/src/main/java/com/example/myjamaat1lad/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.myjamaat1lad;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView LOM = findViewById(R.id.LOM);
+        ArrayList<String> masjids = new ArrayList<>();
+        masjids.add("+ Add a new Masjid");
+        Context context;
+        ArrayAdapter arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, masjids);
+        LOM.setAdapter(arrayAdapter);
+
+
+
     }
 }
